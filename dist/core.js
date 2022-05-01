16d5050a1dc2762abc0bf87df1ba24a69f7babbb
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-05-01 20:12:05 +0900

    Feat : Get wordSeparateAll as Array

--- dist/core.js
+++ dist/core.js
@@ -1,5 +1,5 @@
 const kop = {
-    wordSeparate(kor) {
+    wordSeparate(word) {
         const f = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
                 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
                 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
@@ -12,7 +12,7 @@ const kop = {
                 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
 
         const ga = 44032;
-        let uni = kor.charCodeAt(0);
+        let uni = word.charCodeAt(0);
 
         uni = uni - ga;
 
@@ -28,5 +28,32 @@ const kop = {
             returnInput += this.wordSeparate(input[i]);
         }
         return returnInput;
+    },
+    wordSeparateToArray(word){
+        const f = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
+                'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
+                'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+        const s = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ',
+                'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ',
+                'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+        const t = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ',
+                'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ',
+                'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ',
+                'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+        const ga = 44032;
+
+        let returnWord = [];
+        for(var i = 0;i < word.length;i++){
+            let uni = word.charCodeAt(i);
+            uni = uni - ga;
+
+            let fn = parseInt(uni / 588);
+            let sn = parseInt((uni - (fn * 588)) / 28);
+            let tn = parseInt(uni % 28);
+            returnWord.push([f[fn] , s[sn] , t[tn]]);
+        };
+
+        return returnWord;
     }
 };
