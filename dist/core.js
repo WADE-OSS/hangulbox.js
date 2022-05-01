492b5064fd6cfde58c2c7c235a12e807573f72ba
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-05-01 14:54:45 +0900

    Create core.js

---
+++ dist/core.js
@@ -0,0 +1,32 @@
+const kop = {
+    wordSeparate(kor) {
+        const f = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
+                'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
+                'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+        const s = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ',
+                'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ',
+                'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+        const t = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ',
+                'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ',
+                'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ',
+                'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+        const ga = 44032;
+        let uni = kor.charCodeAt(0);
+
+        uni = uni - ga;
+
+        let fn = parseInt(uni / 588);
+        let sn = parseInt((uni - (fn * 588)) / 28);
+        let tn = parseInt(uni % 28);
+
+        return f[fn] + s[sn] + t[tn];
+    },
+    wordSeparateAll(input){
+        let returnInput = "";
+        for(var i = 0;i < input.length;i++){
+            returnInput += this.wordSeparate(input[i]);
+        }
+        return returnInput;
+    }
+};
